f79c5ac414fb579cc68b73779dc2036c3bed41e5
Vili Kinnunen <vili.kinnunen1@gmail.com>
2017-11-11 20:21:04 +0200

    Add react-router-dom.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {HashRouter, Route, Switch} from 'react-router-dom';
 
 import './App.scss';
 
@@ -8,6 +9,13 @@ export class App extends React.Component {
             <header>
                 <h1>Futu Gallery</h1>
             </header>
+
+            <HashRouter>
+                <Switch>
+                    <Route exact path="/" render={() => <h2>Multiple items here</h2>}/>
+                    <Route path="/picture/:id" render={() => <h2>One item here</h2>}/>
+                </Switch>
+            </HashRouter>
         </div>
     }
 }
